d1549be14fde3dbf62839f816710a7ab3147b471
vallepuv <vineel.vallepu@news.com.au>
2016-08-20 23:21:29 +1000

    updated the tests to match acceptance criteria

--- demo_test_page_objects/admin_page.py
+++ demo_test_page_objects/admin_page.py
@@ -8,6 +8,8 @@ class AdminPage(AppiumLibrary):
         This function clicks on refresh icon available on the app
         :return: current page object
         """
+        self.wait_until_page_contains_element("id=com.ustwo.sample:id/commit_list_button_refresh", 10,
+                                              "Could not find refresh element")
         self.click_element("id=com.ustwo.sample:id/commit_list_button_refresh")
         return self
 
@@ -17,7 +19,6 @@ class AdminPage(AppiumLibrary):
         :return: List
         """
         try:
-
             self.wait_until_page_contains_element("id=com.ustwo.sample:id/commit_list_row_textview_message", 10,
                                                   "Could not find commit message")
             elements = self.get_elements("id=com.ustwo.sample:id/commit_list_row_textview_message")
@@ -29,3 +30,123 @@ class AdminPage(AppiumLibrary):
             print known_exception
             return []
 
+    def get_commit_at_index(self, index):
+        """
+        This function returns available commits
+        :param index: index of the commit
+        :return: List
+        """
+        self.wait_until_page_contains_element("id=com.ustwo.sample:id/commit_list_row_textview_message", 10,
+                                              "Could not find commit message")
+        elements = self.get_elements("id=com.ustwo.sample:id/commit_list_row_textview_message")
+        for element_index, element in enumerate(elements):
+            if str(index) == str(element_index+1):
+                return element.text
+        raise ValueError('Could not find element at index %s' % index)
+
+    def get_available_projects(self):
+        """
+        This function returns available projects
+        :return: List
+        """
+        try:
+            self.wait_until_page_contains_element("id=com.ustwo.sample:id/commit_list_textview_title", 10,
+                                                  "Could not find commit message")
+            elements = self.get_elements("id=com.ustwo.sample:id/commit_list_textview_title")
+            available_commits = []
+            for element in elements:
+                available_commits.append(element.text)
+            return available_commits
+        except AssertionError as known_exception:
+            print known_exception
+            return []
+
+    def get_timestamp_for_commit(self, commit_name):
+        """
+        This function returns timestamp for given commit name
+        :param commit_name: Commit name for which the timestamp is requested
+        :return: String
+        """
+        available_commits = self.get_available_commits()
+        for index, commit in enumerate(available_commits):
+            if commit.lower() == commit_name.lower():
+                return self.get_elements(
+                    "xpath=(//android.widget.LinearLayout[%s]/android.widget.TextView[2])" % (index+1))[0].text
+        raise ValueError('Unable to find commit %s' % commit_name)
+
+    def click_on_commit(self, commit_name):
+        """
+        This function clicks on requested commit
+        :param commit_name: Commit name for which the timestamp is requested
+        :return: self
+        """
+        available_commits = self.get_available_commits()
+        for index, commit in enumerate(available_commits):
+            if commit.lower() == commit_name.lower():
+                self.click_element("xpath=(//android.widget.LinearLayout[%s])" % (index+1))
+                return self
+        raise ValueError('Unable to find commit %s' % commit_name)
+
+    def get_project_name(self):
+        """
+        This function returns the project title displayed in app
+        :return: string
+        """
+        self.wait_until_page_contains_element("id=com.ustwo.sample:id/action_bar", 10, "Could not find action bar")
+        elements = self.get_elements("xpath=(//android.widget.TextView[1])")
+        for sub_element in elements:
+            return sub_element.text
+
+    def get_author_name(self):
+        """
+        This function returns the author name for the commit selected
+        :return: string
+        """
+        self.wait_until_page_contains_element("id=com.ustwo.sample:id/action_bar", 10, "Could not find action bar")
+        return self.get_element_attribute("id=com.ustwo.sample:id/commit_detail_textview_name", "text")
+
+    def get_author_email(self):
+        """
+        This function returns the author email for the commit selected
+        :return: string
+        """
+        self.wait_until_page_contains_element("id=com.ustwo.sample:id/action_bar", 10, "Could not find action bar")
+        return self.get_element_attribute("id=com.ustwo.sample:id/commit_detail_textview_email", "text")
+
+    def get_commit_date(self):
+        """
+        This function returns the author email for the commit selected
+        :return: string
+        """
+        self.wait_until_page_contains_element("id=com.ustwo.sample:id/action_bar", 10, "Could not find action bar")
+        return self.get_element_attribute("id=com.ustwo.sample:id/commit_detail_textview_date", "text")
+
+    def get_commit_message(self):
+        """
+        This function returns the author email for the commit selected
+        :return: string
+        """
+        self.wait_until_page_contains_element("id=com.ustwo.sample:id/action_bar", 10, "Could not find action bar")
+        return self.get_element_attribute("id=com.ustwo.sample:id/commit_detail_textview_message", "text")
+
+    def is_private_project(self):
+        """
+        This function returns the author email for the commit selected
+        :return: string
+        """
+        self.wait_until_page_contains_element("id=com.ustwo.sample:id/commit_list_row_textview_message", 10,
+                                              "Could not find commit message")
+        if self._is_element_present("id=com.ustwo.sample:id/commit_list_imageview_privacy_state"):
+            return False
+        return True
+
+    def is_public_project(self):
+        """
+        This function returns the author email for the commit selected
+        :return: string
+        """
+        self.wait_until_page_contains_element("id=com.ustwo.sample:id/commit_list_row_textview_message", 10,
+                                              "Could not find commit message")
+        if self._is_element_present("id=com.ustwo.sample:id/commit_list_imageview_privacy_state"):
+            return True
+        return False
--- tests/test_app.robot
+++ tests/test_app.robot
@@ -1,5 +1,6 @@
 *** Settings ***
 Library           admin_page.AdminPage  WITH NAME  AdminPage
+Test Teardown     Close All Applications
 
 *** Variables ***
 ${REMOTE_URL}     http://localhost:4723/wd/hub
@@ -9,13 +10,52 @@ ${DEVICE_NAME}    Android Emulator
 ${APP}            ${CURDIR}/apps/app-sample.apk
 
 *** Keywords ***
-add new contact
+Open Application On Mobile
     Open Application  http://localhost:4723/wd/hub  platformName=Android  platformVersion=6.0  deviceName=192.168.56.101:5555  app=${CURDIR}/apps/app-sample.apk  automationName=appium  appPackage=com.ustwo.sample  appActivity=CommitListActivity
-    Capture Page Screenshot
-    Sleep  10
-    Get Available Commits
+
+Open Application And Verify Project Information
+    Open Application On Mobile
+    ${available_projects} =  Get Available Projects
+    ${available_commits} =  Get Available Commits
+    ${project_status} =  Is Public Project
+    Should Be True  ${project_status}
+
+Open Application And Verify Latest Project Activity
+    [Documentation]  This keyword is intentionally made a negative test case, assuming no latest activity, otherwise it will pass
+    Open Application On Mobile
+    ${ref_commit} =  Get Commit At Index  1
     Click Refresh
+    ${latest_commit} =  Get Commit At Index  1
+    Should Not Be Equal  ${ref_commit}  ${latest_commit}
+
+Open Application And Verify Commit Information
+    Open Application On Mobile
+    ${ref_commit} =  Get Commit At Index  3
+    ${ref_timestamp} =  Get Timestamp For Commit  ${ref_commit}
+    Click On Commit  ${ref_commit}
+    ${proj_name} =  Get Project Name
+    Should Be Equal  ${proj_name}  Full Cycle App Testing Sample
+    ${author_name} =  Get Author Name
+    Should Be Equal  ${author_name}  Martin Stolz
+    ${author_email} =  Get Author Email
+    Should Be Equal  ${author_email}  martin@ustwo.co.uk
+    ${commit_date} =  Get Commit Date
+    Should Be Equal  ${commit_date}  2013-11-27T13:11:12Z
+    ${commit_message} =  Get Commit Message
+    Should Be Equal  ${commit_message}  Modified email recipients
 
 *** Test Cases ***
-Test Case One
-    add new contact
+User Should Be Able To Access Project Information
+    [Tags]  Demo Test  Acceptance Criteria  TestCase01
+    [Documentation]  AC: Display project information (title, public / private, etc)
+    Open Application And Verify Project Information
+
+User Should Be Able To View the Latest Project Activity
+    [Tags]  Demo Test  Acceptance Criteria  TestCase02
+    [Documentation]  AC: Display latest project activity
+    Open Application And Verify Latest Project Activity
+
+User Should Be Able To View Additional Information For Any Desired Commit
+    [Tags]  Demo Test  Acceptance Criteria  TestCase03
+    [Documentation]  AC: Display commit information
+    Open Application And Verify Commit Information
